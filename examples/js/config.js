fcce635bb92d203ca5cbeca90450b3f5d1e55561
mousaviasil <40757161+mousaviasil@users.noreply.github.com>
2020-01-29 07:14:05 +0330

    test my site

--- examples/js/config.js
+++ examples/js/config.js
@@ -11,12 +11,12 @@ var config = {
   // If Github mode is set, your Github username, repo name,
   // and branch to get files from.
   github: {
-    username: 'yourusername',
-    repo: 'yourrepo',
-    branch: 'gh-pages',
+    username: 'mousaviasil',
+    repo: 'TedamisCms.js',
+    branch: 'tedamis-gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
-    // prefix: 'subdirectory',
+     prefix: 'examples',
   },
 
   // The name of the layouts directory.
